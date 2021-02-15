17e70d1cfdf72df7e28c6fbeb3c2496f5621386f
Rosie Choi <rosiechoimanso@gmail.com>
2021-02-15 18:19:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,5 +23,14 @@ def initialize(name, grade, id=nil)
      DB[:conn].execute(sql)
    end
 
+   def save
+       sql = <<-SQL
+         INSERT INTO students (name, grade) 
+         VALUES (?, ?)
+       SQL
+    
+       DB[:conn].execute(sql, self.name, self.grade)
+    
+     end
 
 end
