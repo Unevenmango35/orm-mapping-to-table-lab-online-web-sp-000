15e7b90c1c3ffc7bfff3e9a505dfca6ad82b8d3c
Rosie Choi <rosiechoimanso@gmail.com>
2021-02-15 18:12:59 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 require 'bundler'
 Bundler.require
-
-require_relative '../lib/student'
 require 'sqlite3'
+require_relative '../lib/student'
+
 
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
